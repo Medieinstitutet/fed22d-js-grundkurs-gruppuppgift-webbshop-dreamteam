0c8a4073b7cfff5b3eecadc0f00655962ae7d6d4
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2022-11-24 10:58:47 +0100

    fix dark and light mode

--- script.js
+++ script.js
@@ -85,6 +85,20 @@ const donuts = [
 const darkMode = document.getElementById('dark-mode');
 const lightMode = document.getElementById('light-mode');
 
+darkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark_mode')
+  darkMode.classList.toggle('hide')
+  lightMode.classList.remove('hide')
+
+})
+
+lightMode.addEventListener('click', () => {
+  document.body.classList.remove('dark_mode')
+  lightMode.classList.toggle('hide')
+  darkMode.classList.remove('hide')
+})
+
+
 
 const donutContainer = document.querySelector('#allDonuts');
 
@@ -434,17 +448,5 @@ function showDivs(n) {
         }
       }
 
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark_mode')
-    darkMode.classList.toggle('hide')
-    lightMode.classList.remove('hide')
-
-})
-
-lightMode.addEventListener('click', () => {
-    document.body.classList.remove('dark_mode')
-    lightMode.classList.toggle('hide')
-    darkMode.classList.remove('hide')
-})
 
 
